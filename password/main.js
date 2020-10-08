cdb24c96342121b9b8c84ab27f5f0733328a5cbd
Vincent Letourneau <vincent@nanoninja.com>
2020-10-08 12:14:29 +0200

    Add options event

--- password/main.js
+++ password/main.js
@@ -2,20 +2,40 @@ const p = new Password();
 p.exclude(Password.symbols);
 
 const options = document.querySelector('#options');
-const size = document.querySelector('#size');  // <input id="size">
+const size = document.querySelector('#size');
 const slider = document.querySelector('#slider');
+const displayer = document.querySelector('#displayer');
 
-// addEventListener('type', callback, boolean: direction de l'événement);
-// size.addEventListener('input', () => slider.value = size.value );
+refresh();
 
-size.addEventListener('input', event => {
-    // console.log(event.target === size);
-    // console.log(event.target.value);
-    // console.log(size.value);
+size.addEventListener('input', () => {
     slider.value = size.value;
+    refresh();
 });
 
-rangeFormList();
+slider.addEventListener('input', () => {
+    size.value = slider.value;
+    refresh();
+});
+
+options.addEventListener('click', evt => {
+    const elem = evt.target;
+
+    if (elem.type && elem.type === 'checkbox') {
+        if (elem.checked) {
+            p.include(elem.value);
+        } else {
+            p.exclude(elem.value);
+        }
+
+        refresh();
+    }
+});
+
+function refresh() {
+    displayer.textContent = p.generate(size.value);
+    rangeFormList();
+}
 
 function rangeFormList() {
     options.innerHTML = '';
